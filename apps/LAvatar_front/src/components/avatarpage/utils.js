9bd562b9aa10d459f80dc8bd2eae0ff792535f4b
izjoker <izjoker3@gmail.com>
2023-02-14 11:36:26 +0900

    ms to formatted period function

--- apps/LAvatar_front/src/components/avatarpage/utils.js
+++ apps/LAvatar_front/src/components/avatarpage/utils.js
@@ -1,9 +1,22 @@
-import classLst from '../../assets/classLst.json'
-export const checkRootClass = (className) => {
-    console.log(classLst)
-    if (classLst['rootClasses'].includes(className)){
-        return true
-    }else{
-        return false
-    }
+export function convertMsToTime(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
+    let months = Math.floor(days / 30);
+    const years = Math.floor(days / 365);
+
+    seconds = seconds % 60;
+    minutes = minutes % 60;
+    hours = hours % 24;
+    days = days % 30;
+    months = months % 12;
+    const r = {};
+
+    r['years'] = years;
+    r['months'] = months;
+    r['days'] = days;
+    r['hours'] = hours;
+    r['minutes'] = minutes;
+    return r;
 }
